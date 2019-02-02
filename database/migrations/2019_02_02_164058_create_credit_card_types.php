bf76fdf030024651d70ed2cec70346ac3af439ef
Justin Navarro <info@justinnavarro.net>
2019-02-02 12:36:58 -0500

    create bank, credit card, investment, and utility types

---
+++ database/migrations/2019_02_02_164058_create_credit_card_types.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCreditCardTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('credit_card_types')) {
+            Schema::create('credit_card_types', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('slug');
+                $table->timestamps();
+            });
+
+            DB::table('credit_card_types')->insert([
+                ['name' => 'MasterCard', 'slug' => 'mastercard', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
+                ['name' => 'Visa', 'slug' => 'visa', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
+                ['name' => 'Discover', 'slug' => 'discover', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
+                ['name' => 'American Express', 'slug' => 'american_express', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credit_card_types');
+    }
+}
