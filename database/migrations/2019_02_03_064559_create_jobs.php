d1e3fd6a569c35bc103fdf6061f71af79399ef37
Justin Navarro <info@justinnavarro.net>
2019-02-05 08:27:49 -0500

    update jobs migration

--- database/migrations/2019_02_03_064559_create_jobs.php
+++ database/migrations/2019_02_03_064559_create_jobs.php
@@ -18,12 +18,42 @@ class CreateJobs extends Migration
                 $table->increments('id');
                 $table->string('name');
                 $table->string('amount');
+                $table->integer('budget_id', false, 'unsigned');
                 $table->integer('job_type_id', false, 'unsigned');
                 $table->dateTime('initial_pay_date');
                 $table->timestamps();
             });
 
             Schema::table('jobs', function($table) {
+                $table->foreign('budget_id')
+                    ->references('id')
+                    ->on('budgets')
+                    ->onDelete('cascade');
+
+                $table->foreign('job_type_id')
+                    ->references('id')
+                    ->on('job_types')
+                    ->onDelete('cascade');
+            });
+        }
+
+        if (!Schema::hasTable('job_templates')) {
+            Schema::create('job_templates', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('amount');
+                $table->integer('budget_template_id', false, 'unsigned');
+                $table->integer('job_type_id', false, 'unsigned');
+                $table->dateTime('initial_pay_date');
+                $table->timestamps();
+            });
+
+            Schema::table('job_templates', function($table) {
+                $table->foreign('budget_template_id')
+                    ->references('id')
+                    ->on('budget_templates')
+                    ->onDelete('cascade');
+
                 $table->foreign('job_type_id')
                     ->references('id')
                     ->on('job_types')
