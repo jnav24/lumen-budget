e245193c22ffd5fb60d070613fd203fd6accf53c
Justin Navarro <info@justinnavarro.net>
2019-03-16 14:36:23 -0400

    create global helper and forget password mailable

---
+++ app/Helpers/GlobalHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Helpers;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Str;
+
+class GlobalHelper
+{
+    /**
+     * Return now if date time is null
+     *
+     * @param Carbon|null $dateTime
+     * @param string $timeZone
+     *
+     * @return Carbon
+     */
+    public static function setDefaultDateTimeIfNull($dateTime = null, string $timeZone = 'UTC'): Carbon
+    {
+        return $dateTime ?? Carbon::now($timeZone);
+    }
+
+    /**
+     * Generate a random string
+     *
+     * @param int $length
+     * @return string
+     */
+    public static function generateToken(int $length = 40)
+    {
+        return hash_hmac('sha256', Str::random($length), env('APP_KEY', '$ySt3MOfTh3D0wn!22'));
+    }
+
+    /**
+     * Send Email
+     *
+     * @param string $to
+     * @param Mailable $mailable
+     * @return bool
+     */
+    public static function sendMailable($to, $mailable)
+    {
+        try {
+            Mail::to($to)->send($mailable);
+            return true;
+        } catch(\Exception $ex) {
+            return false;
+        }
+    }
+}
---
+++ app/Mail/ForgotPasswordMailable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Helpers\GlobalHelper;
+
+class ForgotPasswordMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The user instance.
+     *
+     * @var $user
+     */
+    public $user;
+
+    /**
+     * ForgotPasswordMailable constructor.
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+        $this->user->password_reset_token = GlobalHelper::generateToken(40);
+        $this->user->password_reset_expires = GlobalHelper::setDefaultDateTimeIfNull()->addHour();
+        $this->user->save();
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject(env('APP_NAME', 'System') . ' password reset')
+            ->from(env('MAIL_FROM_ADDRESS'), env('APP_NAME'))
+            ->view('emails.auth.forgotpassword');
+    }
+}
