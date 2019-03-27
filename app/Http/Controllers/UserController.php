875672a2ddb1b224e3dd55f18226843d4bdbcf26
Justin Navarro <info@justinnavarro.net>
2019-03-26 22:32:29 -0400

    save user profile data and set up for saving user vehicles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserProfile;
 use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
+    protected $tableId = 'user_id';
+
     public function updateUserProfile()
     {
         try {
@@ -15,11 +18,24 @@ class UserController extends Controller
             ]);
 
             $profileAttributes = ['first_name', 'last_name'];
-            $vehicleAttributes = ['make', 'model', 'year', 'color', 'license'];
+            $profileRequest = array_intersect_key($this->request->input('profile'), array_flip($profileAttributes));
+
+            if (empty($profileRequest) || count($profileAttributes) !== count($profileRequest)) {
+                return $this->respondWithBadRequest([], '');
+            }
+
+            $profile = UserProfile::where('user_id', $this->request->auth->id)->first();
+            $profile->first_name = $profileRequest['first_name'];
+            $profile->last_name = $profileRequest['last_name'];
+            $profile->save();
+
+            $vehicleAttributes = ['id', 'make', 'model', 'year', 'color', 'license', 'active'];
+            // $vechicles = $this->insertOrUpdate($vehicleAttributes, $this->request->input('vehicles'), $this->request->auth->id, 'user_vehicles');
+            $vehicles = [];
 
             return $this->respondWithOK([
-                'profile' => [],
-                'vehicles' => [],
+                'profile' => array_merge($profile->toArray(), ['email' => $this->request->auth->username]),
+                'vehicles' => $vehicles,
             ]);
         } catch(ValidationException $e) {
             return $this->respondWithBadRequest($e->getMessage(), 'Error validating request');
