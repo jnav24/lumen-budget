679a56518fc2e65aa7dd8242654cae94beaec3f3
Justin Navarro <info@justinnavarro.net>
2019-04-29 17:11:05 -0400

    remove hello

--- routes/web.php
+++ routes/web.php
@@ -65,4 +65,3 @@ $router->group(['middleware' => 'jwt.auth'], function ($router) {
 $router->group(['middleware' => 'jwt.auth'], function ($router) {
    $router->post('user-profile', 'UserController@updateUserProfile');
 });
-// hello?
