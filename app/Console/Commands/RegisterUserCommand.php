b8e38478d0df7bd7158eb19fb75fee232edc38cc
Justin Navarro <info@justinnavarro.net>
2019-11-19 16:44:39 -0500

    created logic to create a new user from command

--- app/Console/Commands/RegisterUserCommand.php
+++ app/Console/Commands/RegisterUserCommand.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class RegisterUserCommand extends Command
 {
@@ -10,5 +14,48 @@ class RegisterUserCommand extends Command
     protected $description = 'Creates a user in the DB';
 
     public function handle()
-    {}
+    {
+        try {
+            DB::transaction(function () {
+                $this->createUser();
+            });
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
+    }
+
+    public function createUser() {
+        $faker = Faker::create();
+
+        if (empty($this->option('email'))) {
+            $this->error('Email is required');
+            exit;
+        }
+
+        if (empty($this->option('password'))) {
+            $this->error('Password is required');
+            exit;
+        }
+
+        if (User::where('username', $this->option('email'))->exists()) {
+            $this->error('User already exists');
+            exit;
+        }
+
+        $user = new User();
+        $profile = new UserProfile();
+
+        $user->username = $this->option('email');
+        $user->password = app('hash')->make($this->option('password'));
+        $user->save();
+
+        $profile->user_id = $user->id;
+        $profile->image = '';
+        $profile->first_name = $this->option('first') ?? $faker->firstName;
+        $profile->last_name = $this->option('last') ?? $faker->lastName;
+        $profile->save();
+
+        $this->info('User created');
+    }
 }
