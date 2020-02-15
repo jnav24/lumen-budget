356a0e98037f0796ba928fc288ed90cea858ee04
Justin Navarro <info@justinnavarro.net>
2020-02-15 11:36:45 -0500

    styling the email templates

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -72,12 +72,16 @@
         }
     </style>
 </head>
-<body class="" style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
-<table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;">
+<body class="" style="background-color: #45ADA8; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
+<table border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #45ADA8;">
     <tr>
         <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp;</td>
         <td class="container" style="font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; Margin: 0 auto; max-width: 580px; padding: 10px; width: 580px;">
             <div class="content" style="box-sizing: border-box; display: block; Margin: 0 auto; max-width: 580px;">
+
+                <div style="margin: 20px auto; text-align: center;">
+                    <img src="https://dimebudget.app/img/logo_1.800ef77a.png" alt="" style="width: 168px">
+                </div>
 
                 <!-- START CENTERED WHITE CONTAINER -->
                 <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">Welcome</span>
@@ -89,11 +93,11 @@
                 </table>
 
                 <!-- START FOOTER -->
-                <div class="footer" style="clear: both; Margin-top: 10px; text-align: center; width: 100%;">
+                <div class="footer" style="clear: both; margin-top: 10px; text-align: center; width: 100%;">
                     <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                         <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">{{ env('COMPANY', 'Company') }}, {{ env('COMPANY_ADDRESS', 'Address') }}</span>
+                                <span class="apple-link" style="color: #fff; font-size: 12px; text-align: center;">&copy; Dime Budget {{ date('Y') }}</span>
                             </td>
                         </tr>
                     </table>
