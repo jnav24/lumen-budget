2382f36b0a4c687ac6db907b7b51c64b86fbeb0d
Justin Navarro <info@justinnavarro.net>
2020-07-18 07:31:33 -0400

    add new bank types

---
+++ database/migrations/2020_07_18_112356_add_bank_types.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBankTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('bank_types')->insert([
+            ['name' => 'Money Market Account (MMA)', 'slug' => 'mma', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
+            ['name' => 'Certificate of Deposit (CD)', 'slug' => 'cd', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
