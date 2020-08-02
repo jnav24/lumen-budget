582c8bf16f62ded74a28787fd4d68e7898096770
Justin Navarro <info@justinnavarro.net>
2020-08-02 15:12:53 -0400

    rename bill type jobs to incomes

---
+++ database/migrations/2020_08_02_191023_rename_bill_type_job.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameBillTypeJob extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Illuminate\Support\Facades\DB::table('bill_types')->where('slug', 'jobs')->update([
+            'name' => 'Incomes',
+            'slug' => 'incomes',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
