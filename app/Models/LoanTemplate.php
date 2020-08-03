6c6eaab1256f5e98a661e5ccda71110b4eb1b678
Justin Navarro <info@justinnavarro.net>
2020-08-03 11:14:36 -0400

    add loan models

---
+++ app/Models/LoanTemplate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LoanTemplate extends Model
+{
+    /**
+     * Hide columns
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'budget_template_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'loan_templates';
+}
