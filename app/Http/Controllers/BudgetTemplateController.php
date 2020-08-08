9adea25a8c71df7cab1a1436918cfa8da27d42bc
Justin Navarro <info@justinnavarro.net>
2020-08-08 14:29:19 -0400

    refactor budget template save

--- app/Http/Controllers/BudgetTemplateController.php
+++ app/Http/Controllers/BudgetTemplateController.php
@@ -85,34 +85,11 @@ class BudgetTemplateController extends Controller
                 return $this->respondWithBadRequest([], 'Missing expenses.');
             }
 
-            $template = BudgetTemplate::firstOrCreate(
-                [
-                    'user_id' => $this->request->auth->id,
-                ],
-                [
-                    'updated_at' => Carbon::now(),
-                ]
-            );
+            $template = BudgetTemplate::firstOrCreate([
+                'user_id' => $this->request->auth->id,
+            ]);
 
-//            $template = BudgetTemplates::where('user_id', $this->request->auth->id)->first();
-//
-//            if (empty($template)) {
-//                $template = new BudgetTemplates();
-//                $template->created_at = Carbon::now();
-//            }
-//
-//            $template->user_id = $this->request->auth->id;
-//            $template->updated_at = Carbon::now();
-//            $template->save();
-            $savedData = [];
-//
-//            foreach ($this->request->input('expenses') as $type => $expenses) {
-//                $method = $type . '_templates';
-//
-//                if (method_exists($this, $method)) {
-//                    $savedData[$type] = $this->{$method}($expenses, $template->id);
-//                }
-//            }
+            $savedData = $this->saveExpenses($template->id, $this->request->input('expenses'), true);
 
             return $this->respondWithOK([
                 'templates' => [
@@ -124,112 +101,5 @@ class BudgetTemplateController extends Controller
             Log::error('BudgetTemplateController::saveBudgetTemplates - ' . $e->getMessage());
             return $this->respondWithBadRequest([], 'Unable to save budget template at this time.');
         }
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function banks_templates($expenses, $id)
-    {
-        $attributes = $this->getBanksAttributes();
-        if (($key = array_search('bank_template_id', $attributes)) !== false) {
-            unset($attributes[$key]);
-        }
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'bank_templates');
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function credit_cards_templates($expenses, $id)
-    {
-        $attributes = $this->getCreditCardsAttributes();
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'credit_card_templates');
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function investments_templates($expenses, $id)
-    {
-        $attributes = $this->getInvestmentAttributes();
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'investment_templates');
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function jobs_templates($expenses, $id)
-    {
-        $attributes = $this->getJobsAttributes();
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'job_templates');
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function medical_templates($expenses, $id)
-    {
-        $attributes = $this->getMedicalAttributes();
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'medical_templates');
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function miscellaneous_templates($expenses, $id)
-    {
-        $attributes = $this->getMiscellaneousAttributes();
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'miscellaneous_templates');
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function utilities_templates($expenses, $id)
-    {
-        $attributes = $this->getUtilitiesAttributes();
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'utility_templates');
-    }
-
-    /**
-     * Dynamic method called from saveBudgetTemplates()
-     *
-     * @param $expenses
-     * @param $id
-     * @return array
-     */
-    private function vehicles_templates($expenses, $id)
-    {
-        $attributes = $this->getVehiclesAttributes();
-        return $this->insertOrUpdate($attributes, $expenses, $id, 'vehicle_templates');
     }
 }
