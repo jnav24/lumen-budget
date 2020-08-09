323e8d841c1c7bee6bfb4b3fd606290b068e455a
Justin Navarro <info@justinnavarro.net>
2020-08-09 12:04:46 -0400

    consolidate call to get types for all bill types

--- app/Http/Controllers/TypesController.php
+++ app/Http/Controllers/TypesController.php
@@ -14,21 +14,6 @@ use App\Models\VehicleTypes;
 class TypesController extends Controller
 {
     /**
-     * Get Bank Types
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
-     */
-    public function bank()
-    {
-        try {
-            $types = ['types' => BankTypes::all()->toArray()];
-            return $this->respondWithOK($types);
-        } catch (\Exception $e) {
-            return $this->respondWithBadRequest([], 'Unable to retrieve bank types at this time.');
-        }
-    }
-
-    /**
      * Get Bill Types
      *
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
@@ -36,100 +21,23 @@ class TypesController extends Controller
     public function bill()
     {
         try {
-            $types = ['types' => BillTypes::orderBy('slug')->get()->toArray()];
-            return $this->respondWithOK($types);
+            $billTypes = BillTypes::orderBy('slug')->get();
+            $types = [];
+
+            $billTypes->each(function ($type) use (&$types) {
+                $model = 'App\\Models\\' . $type->model . 'Type';
+
+                if (class_exists($model)) {
+                    $types[$type->slug] = $model::all();
+                }
+            });
+
+            return $this->respondWithOK([
+                'bill_types' => $billTypes,
+                'types' => $types,
+            ]);
         } catch (\Exception $e) {
             return $this->respondWithBadRequest([], 'Unable to retrieve bill types at this time.');
-        }
-    }
-
-    /**
-     * Get Credit Card Types
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
-     */
-    public function creditCard()
-    {
-        try {
-            $types = ['types' => CreditCardTypes::all()->toArray()];
-            return $this->respondWithOK($types);
-        } catch (\Exception $e) {
-            return $this->respondWithBadRequest([], 'Unable to retrieve credit card types at this time.');
-        }
-    }
-
-    /**
-     * Get Investment Types
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
-     */
-    public function investment()
-    {
-        try {
-            $types = ['types' => InvestmentTypes::all()->toArray()];
-            return $this->respondWithOK($types);
-        } catch (\Exception $e) {
-            return $this->respondWithBadRequest([], 'Unable to retrieve investment types at this time.');
-        }
-    }
-
-    /**
-     * Get Job Types
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
-     */
-    public function job()
-    {
-        try {
-            $types = ['types' => IncomeType::all()->toArray()];
-            return $this->respondWithOK($types);
-        } catch (\Exception $e) {
-            return $this->respondWithBadRequest([], 'Unable to retrieve job types at this time.');
-        }
-    }
-
-    /**
-     * Get Medical Types
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
-     */
-    public function medical()
-    {
-        try {
-            $types = ['types' => MedicalTypes::all()->toArray()];
-            return $this->respondWithOK($types);
-        } catch (\Exception $e) {
-            return $this->respondWithBadRequest([], 'Unable to retrieve medical types at this time.');
-        }
-    }
-
-    /**
-     * Get Utility Types
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
-     */
-    public function utility()
-    {
-        try {
-            $types = ['types' => UtilityTypes::all()->toArray()];
-            return $this->respondWithOK($types);
-        } catch (\Exception $e) {
-            return $this->respondWithBadRequest([], 'Unable to retrieve utility types at this time.');
-        }
-    }
-
-    /**
-     * Get Vehicle Types
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
-     */
-    public function vehicle()
-    {
-        try {
-            $types = ['types' => VehicleTypes::all()->toArray()];
-            return $this->respondWithOK($types);
-        } catch (\Exception $e) {
-            return $this->respondWithBadRequest([], 'Unable to retrieve vehicle types at this time.');
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,7 @@ $router->group(['middleware' => 'jwt.auth'], function ($router) {
  * Types
  */
 $router->group(['middleware' => 'jwt.auth'], function ($router) {
-    $router->get('types/bank', 'TypesController@bank');
     $router->get('types/bill', 'TypesController@bill');
-    $router->get('types/credit-card', 'TypesController@creditCard');
-    $router->get('types/investment', 'TypesController@investment');
-    $router->get('types/job', 'TypesController@job');
-    $router->get('types/medical', 'TypesController@medical');
-    $router->get('types/utility', 'TypesController@utility');
-    $router->get('types/vehicle', 'TypesController@vehicle');
 });
 
 /**
